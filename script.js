190e4628b19cbcd142c7192e27bd0a3b0cb11f29
natasha alkmim <natashaalkmim@MacBook-Pro-de-natasha.local>
2025-12-22 07:48:45 -0300

    fix button style

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 const backToTop = document.getElementById("backToTop");
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 300) {
-    backToTop.classList.add("show");
-  } else {
-    backToTop.classList.remove("show");
-  }
+  if (window.scrollY > 300) backToTop.classList.add("show");
+  else backToTop.classList.remove("show");
 });
 
 backToTop.addEventListener("click", () => {
