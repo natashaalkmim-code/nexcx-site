826fdaf8d86032c2c226463defd479b406b5e26e
natasha alkmim <natashaalkmim@MacBook-Pro-de-natasha.local>
2025-12-21 21:36:38 -0300

    update layout + button

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
 const backToTop = document.getElementById("backToTop");
 
-// mostrar/esconder ao rolar
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 300) {
-    backToTop.classList.add("show");
-  } else {
-    backToTop.classList.remove("show");
-  }
+  if (window.scrollY > 300) backToTop.classList.add("show");
+  else backToTop.classList.remove("show");
 });
 
-// voltar ao topo
 backToTop.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
+  window.scrollTo({ top: 0, behavior: "smooth" });
 });
